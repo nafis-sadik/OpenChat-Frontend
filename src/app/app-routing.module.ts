1b73e90df845e0b724fd0f586923b8566ba2159c
Md. Nafis Sadik <nafis.sadik01@gmail.com>
2022-01-31 14:46:48 +0600

    Working on chat page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'user', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+  { path: 'user', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'chat', loadChildren: () => import('./chatting/chatting.module').then(m => m.ChattingModule) }
 ];
 
 @NgModule({
---
+++ src/app/chatting/chatting.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatComponent } from './chat/chat.component';
+import { RouterModule } from '@angular/router';
+
+export const routes = [
+  { path : 'chat', component: ChatComponent, pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [
+    ChatComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ChattingModule { }
